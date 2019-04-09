f969ad676e8a21992df883e95fa01ef9dd22205d
ChaseChoi <caiguihao@gmail.com>
2019-04-09 11:00:19 +0800

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Chase Choi on 2019/4/9.
+//  Copyright © 2019 Chase Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
